0a79a4440e29aaa84e5cf7075a6eb1025b615261
Fraser Hamilton <fh21@hw.ac.uk>
2020-01-15 08:18:28 +0000

    its alive

--- index.js
+++ index.js
@@ -7,76 +7,29 @@ const getLog = async () => {
   return logObj.all;
 };
 
-const capture = async (page, i) => {
-  await page.goto("C:/Projects/test-timelapse/index.html");
-  await page.screenshot({
-    path: `capture${i}.png`,
-    fullPage: true
-  });
-};
-
 const run = async () => {
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  //   await page.setViewport({
-  //     width: 1280,
-  //     height: 800
-  //   });
+  await page.setViewport({
+    width: 1280,
+    height: 800
+  });
 
   const logs = await getLog();
 
-  //   await page.goto("C:/Projects/test-timelapse/index.html");
-
-  logs.forEach(async (log, i) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.setViewport({
-      width: 1280,
-      height: 800
-    });
-
+  for (let [i, log] of logs.entries()) {
     await simpleGitPromise.checkout(log.hash);
-
     await page.goto("C:/Projects/test-timelapse/index.html");
-
     await page.screenshot({
       path: `capture${i}.png`,
       fullPage: true
     });
-
-    await browser.close();
-  });
-
-  //   await page.goto("C:/Projects/test-timelapse/index.html");
-
-  //   await page.screenshot({
-  //     path: `capture${12}.png`,
-  //     fullPage: true
-  //   });
-
-  //   await simpleGitPromise.checkout(logs[3].hash);
-
-  //   await page.goto("C:/Projects/test-timelapse/index.html");
-
-  //   await page.screenshot({
-  //     path: `capture${13}.png`,
-  //     fullPage: true
-  //   });
-
-  //   logs.forEach(async (log, i) => {
-  //     await simpleGitPromise.checkout(log.hash);
-  //     await page.screenshot({
-  //       path: `capture${i}.png`,
-  //       fullPage: true
-  //     });
-  //   });
+  }
 
   await simpleGitPromise.checkout("master");
 
-  // close the browser
-  //   await browser.close();
+  await browser.close();
 };
 
 run();
